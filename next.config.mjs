ef6e25c6d0ffdab8f4425d39336d9cf116bb572b
praise24462 <opeyemijosephjoel@gmail.com>
2025-10-04 15:56:20 +0100

    for-netlify

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,20 @@
+// /** @type {import('next').NextConfig} */
+// import withNetlify from '@netlify/plugin-nextjs';
+
+// const nextConfig = {
+//   eslint: {
+//     ignoreDuringBuilds: true,
+//   },
+//   typescript: {
+//     ignoreBuildErrors: true,
+//   },
+//   images: {
+//     unoptimized: true,
+//   },
+// };
+
+// export default withNetlify(nextConfig);
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
@@ -9,6 +26,6 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-}
+};
 
-export default nextConfig
+export default nextConfig;
